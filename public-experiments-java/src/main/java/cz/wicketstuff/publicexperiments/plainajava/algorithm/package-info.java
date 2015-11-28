a6e69ca360b2d5a18a7b177c9663cbf032095c24
strma17 <martin.strejc@ca.com>
2015-11-28 23:35:48 +0100

    Add package info

--- public-experiments-java/src/main/java/cz/wicketstuff/publicexperiments/plainajava/algorithm/package-info.java
+++ public-experiments-java/src/main/java/cz/wicketstuff/publicexperiments/plainajava/algorithm/package-info.java
@@ -1,5 +1,6 @@
 /**
- * @author Martin Strejc
+ * @author <a href="https://cz.linkedin.com/in/martin-strejc-2b317116">Martin Strejc@LinkedIn</a> 
+ * @author <a href="http://stackoverflow.com/users/2018247/martin-strejc?tab=profile">MartinStrejc@StackOverflow</a>
  *
  */
 package cz.wicketstuff.publicexperiments.plainajava.algorithm;
