cb1431e747d08046c61508d442019155f992ab24
strma17 <martin.strejc@ca.com>
2017-01-24 13:39:09 +0100

    Complex test

--- band-search/src/test/java/cz/wicketstuff/publicexperiments/bandsearch/BandSearchTest.java
+++ band-search/src/test/java/cz/wicketstuff/publicexperiments/bandsearch/BandSearchTest.java
@@ -17,9 +17,9 @@ public class BandSearchTest {
 	@Test
 	public void findBandsSize_complexTest() {
 		List<Relation> relations = new LinkedList<>();
+		relations.add(new Relation(4, 3));
 		relations.add(new Relation(1, 2));
 		relations.add(new Relation(2, 3));
-		relations.add(new Relation(4, 3));
 		relations.add(new Relation(4, 1));
 		relations.add(new Relation(2, 1));
 		
